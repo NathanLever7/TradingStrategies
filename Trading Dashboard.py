011289f589482f84731f10682bba63450d2136b1
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-18 15:14:27 +0100

    Update Trading Dashboard.py

--- Trading Dashboard.py
+++ Trading Dashboard.py
@@ -1,2 +1,16 @@
-#Up to date version on Github
+import streamlit as st
+import pandas as pd
+
+# Set the title of the streamlit dashboard
+st.title('Index Trading Strategies')
+
+# Load the CSV file from the GitHub repository (use the raw URL of the CSV file)
+csv_url = 'https://raw.githubusercontent.com/NathanLever7/TradingStrategies/main/VUSA_Metrics.csv'
+data = pd.read_csv(csv_url)
+
+# Print the DataFrame on the streamlit web page
+st.write(data)
+
+
+
 
