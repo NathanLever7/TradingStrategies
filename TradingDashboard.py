62959c74e8870e5dd73b1e8e77fb5d0789237055
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-18 16:34:49 +0100

    Update TradingDashboard.py

--- TradingDashboard.py
+++ TradingDashboard.py
@@ -32,7 +32,7 @@ if selected_stock == 'VUSA (S&P 500)':
     data = pd.read_csv(csv_url)
 
     # 1. Rename the first column and update its values
-    data.rename(columns={data.columns[0]: 'Days Holding Security'}, inplace=True)
+    data.rename(columns={data.columns[0]: 'Days Holding'}, inplace=True)
     data['Days Holding'] = range(1, 11)
     
     # 2. Remove the Average_RMSE column
@@ -46,16 +46,16 @@ if selected_stock == 'VUSA (S&P 500)':
     data.drop(columns=['Average_Actual_Return_Positive', 'Average_Actual_Return_Negative'], inplace=True)
 
     # 5. Rename a column and round its values to 3 decimal places, then add a % sign
-    data.rename(columns={'Average_Actual_Return_Positive_Daily': 'Actual Daily Return when Buying after Positive Prediction'}, inplace=True)
+    data.rename(columns={'Average_Actual_Return_Positive_Daily': 'Daily Return with Positive Prediction Strategy'}, inplace=True)
     data['Actual Daily Return when Buying after Positive Prediction'] = data['Actual Daily Return when Buying after Positive Prediction'].astype(float).round(3).astype(str) + '%'
 
     # 6. Do the equivalent for another column
-    data.rename(columns={'Average_Actual_Return_Negative_Daily': 'Actual Daily Return when Buying after Negative Prediction'}, inplace=True)
+    data.rename(columns={'Average_Actual_Return_Negative_Daily': ''Daily Return with Negative Prediction Strategy'}, inplace=True)
     data['Actual Daily Return when Buying after Negative Prediction'] = data['Actual Daily Return when Buying after Negative Prediction'].astype(float).round(3).astype(str) + '%'
 
     # 7. Rename two more columns
-    data.rename(columns={'Capital_Positive': 'Capital following Positive Prediction Strategy', 
-                         'Capital_Negative': 'Capital following Negative Prediction Strategy'}, inplace=True)
+    data.rename(columns={'Capital_Positive': 'Capital with Positive Prediction Strategy', 
+                         'Capital_Negative': 'Capital with Negative Prediction Strategy'}, inplace=True)
     
     # 8. Rename another column
     data.rename(columns={'Capital_Daily_Investment': 'Capital Investing Every Day'}, inplace=True)
