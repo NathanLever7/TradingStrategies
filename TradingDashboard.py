9682e450ddab8a0539cae508a40acb54fb459777
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-18 15:36:40 +0100

    Update TradingDashboard.py

--- TradingDashboard.py
+++ TradingDashboard.py
@@ -8,5 +8,37 @@ st.title('Index Trading Strategies')
 csv_url = 'https://raw.githubusercontent.com/NathanLever7/TradingStrategies/main/VUSA_Metrics.csv'
 data = pd.read_csv(csv_url)
 
-# Print the DataFrame on the streamlit web page
+# 1. Rename the first column and update its values
+data.rename(columns={data.columns[0]: 'Days Holding Security'}, inplace=True)
+data['Days Holding Security'] = range(1, 11)
+
+# 2. Remove the Average_RMSE column
+data.drop(columns=['Average_RMSE'], inplace=True)
+
+# 3. Move the Average_MAE column to the end and rename it
+average_mae = data.pop('Average_MAE')
+data['Average MAE'] = average_mae
+
+# 4. Remove the specified columns
+data.drop(columns=['Average_Actual_Return_Positive', 'Average_Actual_Return_Negative'], inplace=True)
+
+# 5. Rename a column and add a % sign to its values
+data.rename(columns={'Average_Actual_Return_Positive_Daily': 'Actual Daily Return when Buying after Positive Prediction'}, inplace=True)
+data['Actual Daily Return when Buying after Positive Prediction'] = data['Actual Daily Return when Buying after Positive Prediction'].astype(str) + '%'
+
+# 6. Do the equivalent for another column
+data.rename(columns={'Average_Actual_Return_Negative_Daily': 'Actual Daily Return when Buying after Negative Prediction'}, inplace=True)
+data['Actual Daily Return when Buying after Negative Prediction'] = data['Actual Daily Return when Buying after Negative Prediction'].astype(str) + '%'
+
+# 7. Rename two more columns
+data.rename(columns={'Capital_Positive': 'Capital following Positive Prediction Strategy', 
+                     'Capital_Negative': 'Capital following Negative Prediction Strategy'}, inplace=True)
+
+# 8. Rename another column
+data.rename(columns={'Capital_Daily_Investment': 'Capital Investing Every Day'}, inplace=True)
+
+# 9. Remove the Time_Taken column
+data.drop(columns=['Time_Taken'], inplace=True)
+
+# Print the modified DataFrame on the streamlit web page
 st.write(data)
